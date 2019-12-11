32744005e771c0ffae63405b23b7b9f343c3e103
Brian Colantonio <brianjc87@gmail.com>
2019-12-11 02:48:14 -0800

    create table, add file list of items, add new item, update item, update item conditionally...

---
+++ MoviesItemOps04.js
@@ -0,0 +1,56 @@
+
+/**
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+/*****************************
+// Increment an atomic counter
+ *******************************/
+
+let AWS = require("aws-sdk");
+
+AWS.config.update({
+    region: "us-east-1",
+    //endpoint: "http://localhost:8000"
+});
+
+let docClient = new AWS.DynamoDB.DocumentClient()
+
+let table = "Movies";
+
+let year = 2015;
+let title = "The Big New Movie";
+
+// Increment an atomic counter
+
+let params = {
+    TableName:table,
+    Key:{
+        "year": year,
+        "title": title
+    },
+    UpdateExpression: "set info.rating = info.rating + :val",
+    ExpressionAttributeValues:{
+        ":val": 1
+    },
+    ReturnValues:"UPDATED_NEW"
+};
+
+console.log("Updating the item...");
+docClient.update(params, function(err, data) {
+    if (err) {
+        console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("UpdateItem succeeded:", data);
+    }
+});
